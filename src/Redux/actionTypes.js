822e83285e167f74d96d247776da58e2802bbbf5
joeljojo <joelpamphyl@gmail.com>
2023-03-16 17:28:11 +0300

    Creaded Redux ADD_USER and  LOGIN actionTypes

---
+++ src/Redux/actionTypes.js
@@ -0,0 +1,11 @@
+//  Define types of Actions
+export const ADD_USER = {
+  REQUEST: 'ADD_USER_REQUEST',
+  SUCCESS: 'ADD_USER_SUCCES',
+  FAIL: 'ADD_USER_FAIL',
+};
+export const LOGIN = {
+  RESUEST: 'LODIN_REQUEST',
+  SUCCESS: 'LOGIN_SUCCESS',
+  FAIL: 'LOGIN_FAIL',
+};
