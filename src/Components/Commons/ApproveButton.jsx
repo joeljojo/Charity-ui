4d86b79a4546f53783d5151ede97303d345d43db
joeljojo <joelpamphyl@gmail.com>
2023-03-25 13:47:52 +0300

    Designed RequestsTable component

---
+++ src/Components/Commons/ApproveButton.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function ApproveButton() {
+  return <Button variant="text">Approve</Button>;
+}
---
+++ src/Components/Commons/RejectButton.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function RejectButton() {
+  return <Button variant="text">Reject</Button>;
+}
