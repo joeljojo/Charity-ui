03dc13c6225f4c1464a3497ded26c6f895ae9bc1
joeljojo <joelpamphyl@gmail.com>
2023-03-26 19:31:17 +0300

    Added combineReducers

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,16 @@
 // use of configureStore is recommended. This is for learning purposes
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  combineReducers,
+} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import loginReducer from './reducer';
 
+const rootReducer = combineReducers({ loginState: loginReducer });
 const store = createStore(
-  loginReducer,
+  rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
