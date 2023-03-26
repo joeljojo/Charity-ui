da8cb8b7731cbbc2076137911c86039c951c163c
joeljojo <joelpamphyl@gmail.com>
2023-03-26 20:44:41 +0300

    Added Routing using React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-// import Login from './Components/Login/Login';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
 import './App.css';
 import SignUp from './Components/Register/SignUp';
+import AdminDonorDashboard from './Components/Dashboards/AdminDonorDashboard';
+import ChildrensHome from './Components/Dashboards/ChildrensHome';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="signin" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="userdashboard" element={<ChildrensHome />} />
+        <Route path="admindonordashboard" element={<AdminDonorDashboard />} />
+      </Routes>
     </div>
   );
 }
