36981bb760d111bba944fc503d03f087f09b32e3
joeljojo <joelpamphyl@gmail.com>
2023-04-11 15:47:03 +0300

    Enforced Eslint Rules

--- src/Components/Commons/ApproveButton.jsx
+++ src/Components/Commons/ApproveButton.jsx
@@ -5,31 +5,31 @@ import { useNavigate } from 'react-router-dom';
 import {
   adminApprovesRequest,
   donorApprovesRequest,
-  fetchAdminRequests, 
-  fetchDonorRequests 
+  fetchAdminRequests,
+  fetchDonorRequests,
 } from '../../Redux/actions';
-
 
 export default function ApproveButton(props) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { userID, isDonor, isAdmin } = useSelector((state) => state.loginState.user);
+  const { userID, isDonor, isAdmin } = useSelector(
+    (state) => state.loginState.user
+  );
   const handleApproveRequest = () => {
     const data = props;
     const { id } = data;
-    
-      if (isAdmin) {
-        dispatch(adminApprovesRequest(id));
-        dispatch(fetchAdminRequests())
-      }
-      if (isDonor) {
-        dispatch(donorApprovesRequest(id));
-        dispatch(fetchDonorRequests(userID))
-        setTimeout(() => {
-          navigate('/checkout');
-        }, 1000);
-      }
-  
+
+    if (isAdmin) {
+      dispatch(adminApprovesRequest(id));
+      dispatch(fetchAdminRequests());
+    }
+    if (isDonor) {
+      dispatch(donorApprovesRequest(id));
+      dispatch(fetchDonorRequests(userID));
+      setTimeout(() => {
+        navigate('/checkout');
+      }, 1000);
+    }
   };
   return (
     <Button variant="text" onClick={handleApproveRequest}>
--- src/Components/Commons/RejectButton.jsx
+++ src/Components/Commons/RejectButton.jsx
@@ -1,27 +1,29 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { useSelector, useDispatch } from 'react-redux';
-import { 
+import {
   adminRejectsRequest,
-   donorRejectsRequest, 
-   fetchAdminRequests,
-   fetchDonorRequests } from '../../Redux/actions';
+  donorRejectsRequest,
+  fetchAdminRequests,
+  fetchDonorRequests,
+} from '../../Redux/actions';
 
 export default function RejectButton(props) {
   const dispatch = useDispatch();
-  const { userID, isDonor, isAdmin } = useSelector((state) => state.loginState.user);
+  const { userID, isDonor, isAdmin } = useSelector(
+    (state) => state.loginState.user
+  );
   const handleRejectRequest = () => {
     const data = props;
     const { id } = data;
-      if (isAdmin) {
-        dispatch(adminRejectsRequest(id));
-        dispatch(fetchAdminRequests())
-      }
-      if (isDonor) {
-        dispatch(donorRejectsRequest(id));
-        dispatch(fetchDonorRequests(userID))
-      }
-
+    if (isAdmin) {
+      dispatch(adminRejectsRequest(id));
+      dispatch(fetchAdminRequests());
+    }
+    if (isDonor) {
+      dispatch(donorRejectsRequest(id));
+      dispatch(fetchDonorRequests(userID));
+    }
   };
   return (
     <Button variant="text" onClick={handleRejectRequest}>
